a96cba6b77f4830646537af4e3272073a09aa47c
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2021-05-06 10:41:19 +0200

    go embed

--- edt.go
+++ edt.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "embed"
 	"fmt"
 	"image"
 	"image/color"
@@ -100,104 +101,11 @@ func fatal(e error) {
 	}
 }
 
-var mainpage = `<!DOCTYPE html>
-<html><head><meta charset="utf-8"><title>E</title>
-<link rel="stylesheet" href="/cm.css" type="text/css" />
-<link rel="icon" type="image/png" sizes="48x48" href="/favicon.png">
-<script type="application/javascript" src="/cm.js"></script>
-<style>
-   *{ font-family:monospace; margin:0; padding:0; }
-#but{ color:grey; }
-#hdr{ position:fixed;display:flex;top:0;left:0;right:0; }
-#tag{ background:#FFFFEA; border:1px solid black;}
-#edt{height:auto;top:0;buttom:0;position:fixed;}
-</style>
-</head>
-<body>
-<div id="hdr">
- <button id="but" onclick="wr()" onkey="exec">write</button>
- <input id="tag" style="flex-grow:1"></input>
-</div>
-<textarea id="edt"></textarea>
-<script>
-var tag = document.getElementById("tag")
-var but = document.getElementById("but")
-var edt = document.getElementById("edt")
-var ed = CodeMirror.fromTextArea(edt, {"lineNumbers":true,"tabSize":8,"indentUnit":8,"indentWithTabs":true,"smartIndent":true})
+//go:embed main.html
+var mainpage string
 
-function get(p, f) {
- var r = new XMLHttpRequest()
- r.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { if (f) f(this.response, this); } }
- r.open("GET", p)
- r.send()
-}
-function post(p, f, b) {
- var r = new XMLHttpRequest()
- r.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { if (f) f(this.response, this); } }
- r.open("POST", p)
- r.send(b)
-}
-function hash(s){window.location.hash=encodeURIComponent(s.trim())}
-function clean(x){but.style.color=x?"grey":"black"}
-function rd(file) { 
- get('/r?'+file, function(s){ed.setValue(s); document.title=file;clean(true)})
-}
-function wr() {
- var file = window.location.pathname.substr(1)
- post('/w?'+file, function(s){ if(s.length>0){tag.value=s};clean(true); }, ed.getValue())
-}
-rd(window.location.pathname.substr(1))
+//go:embed cm.js
+var cm_js string
 
-// search selected: middle-button(all), right(next) (bug: mouseup(chrome), ff ok)
-function pd(e){e.preventDefault();e.stopPropagation()}
-document.addEventListener('contextmenu',function(e){e.preventDefault()})
-ed.on('mousedown', function(cm, e) {
- if     (e.button==2 && (ed.getSelection().length>0)){search(ed.getSelection(),false);pd(e)}
- else if(e.button==1 && (ed.getSelection().length>0)){search(ed.getSelection(),true );pd(e)}
-})
-ed.on('change', function(cm, e){clean(false)})
-function indexAll(a, s) { var r = [], i = -1; while ((i = a.indexOf(s, i+1)) != -1){ r.push(i); }; return r; }
-function search(t, all){
- var v = ed.getValue()
- var p = ed.getCursor()
- if(all) {
-  var n = indexAll(v,t)
-  for(var i=0; i<n.length; i++) ed.addSelection(ed.posFromIndex(n[i]), ed.posFromIndex(n[i]+t.length), {"scroll":true})
- } else {
-  var c = ed.indexFromPos(ed.getCursor())
-  c = (p.sticky == "after") ? c+t.length : c
-  var n = v.indexOf(t, c)
-  if (n == -1) { n = v.indexOf(t, 0) }
-  ed.setSelection(ed.posFromIndex(n), ed.posFromIndex(n+t.length), {"scroll":true})
- }
-}
-
-// tag-bar: return(search/goto line/re→replace), mark+button-click: middle(search all), right(search next)
-function goto(line){ ed.setSelection({"line":line,"ch":0},{"line":line}) }
-function tagKey(e){
- if(e.keyCode!=13) return
- var v = tag.value
- if(v.length==0) return
- var line = Number(v)
- if(isNaN(line)==false&&line>=0&&Math.floor(line)==line){goto(line-1);return}
- var i = v.indexOf("→")
- if(i==-1){search(v,false);return}
- var a = v.slice(0,i)
- var b = v.slice(i+1)
- var s = ed.getSelections()
- var re = RegExp(a, "gm")
- for(var i=0; i<s.length; i++) { s[i]=s[i].replace(re,b) }
- ed.replaceSelections(s)
-}
-function tagSelection(){ return tag.value.slice(tag.selectionStart,tag.selectionEnd) }
-function tagMouse(evt){
- var s = tagSelection()
- if     (evt.button==2 && (s.length>0)){search(s,false);evt.preventDefault()}
- else if(evt.button==1 && (s.length>0)){search(s,true );evt.preventDefault()}
-}
-tag.onkeydown = tagKey
-tag.onmousedown = tagMouse
-
-</script>
-</body></html>
-`
+//go:embed cm.css
+var cm_css string
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ktye/edt
+
+go 1.16
